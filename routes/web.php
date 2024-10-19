42f7daed8214d67ea9eb5cd8e3867df2c4815028
rmichelv <richard.villalba.mendieta@gmail.com>
2024-10-19 03:00:48 -0400

    arreglando errores

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,14 @@ use App\Models\User;
 use App\Models\Shift;
 use App\Models\Employee;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
